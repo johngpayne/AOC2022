80ff0c6b84606c5c7c7a1375207c512e9f49225b
John Payne <20407779+johngpayne@users.noreply.github.com>
2022-12-15 21:44:58 +0000

    Day 15

---
+++ Day15.cs
@@ -0,0 +1,52 @@
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AOC
+{
+    [TestData(Value = @"Sensor at x=2, y=18: closest beacon is at x=-2, y=15
+Sensor at x=9, y=16: closest beacon is at x=10, y=16
+Sensor at x=13, y=2: closest beacon is at x=15, y=3
+Sensor at x=12, y=14: closest beacon is at x=10, y=16
+Sensor at x=10, y=20: closest beacon is at x=10, y=16
+Sensor at x=14, y=17: closest beacon is at x=10, y=16
+Sensor at x=8, y=7: closest beacon is at x=2, y=10
+Sensor at x=2, y=0: closest beacon is at x=2, y=10
+Sensor at x=0, y=11: closest beacon is at x=2, y=10
+Sensor at x=20, y=14: closest beacon is at x=25, y=17
+Sensor at x=17, y=20: closest beacon is at x=21, y=22
+Sensor at x=16, y=7: closest beacon is at x=15, y=3
+Sensor at x=14, y=3: closest beacon is at x=15, y=3
+Sensor at x=20, y=1: closest beacon is at x=15, y=3", Result = "26/56000011")]
+    class Day15 : IDay
+    {
+        public string Calc(string input, bool test)
+        {
+            var sensors = new Regex("Sensor at x=(-?\\d+), y=(-?\\d+): closest beacon is at x=(-?\\d+), y=(-?\\d+)")
+                .Matches(input)
+                .Select(m => m.Groups.Values.Skip(1).Select(g => int.Parse(g.Value)).ToArray())
+                .Select(a => (x:a[0], y:a[1], bx:a[2], by:a[3],d:Math.Abs(a[2] - a[0]) + Math.Abs(a[3] - a[1])))
+                .ToArray();
+            var answer1 = sensors.Select(s => (s.x,s.y,s.bx,s.by,s.d,l:s.d - Math.Abs(s.y - (test ? 10 : 2000000))))
+                .Where(s => s.l > 0)
+                .Select(s => 
+                    Enumerable.Range(s.x - s.l, 1 + 2 * s.l)
+                    .Where(x => s.by != (test ? 10 : 2000000) || s.bx != x))
+                    .Aggregate(new List<int>(), (agg, x) => agg.Union(x).ToList()
+                );
+            var answer2 = sensors
+                .Select(s => (s.x,s.y,d:s.d+1))
+                .Select(s => 
+                    Enumerable.Range(s.y - s.d, 1 + 2 * s.d)
+                    .Where(y => y >= 0 && y <= ((test) ? 20 : 4000000))
+                    .Select(y => (x:s.d - Math.Abs(s.y - y), y:y))
+                    .Select(p => new (int x, int y)[] { (x:s.x - p.x, y:p.y), (x:s.x + p.x, y:p.y)}.Where(p => p.x >= 0 && p.x <= ((test) ? 20 : 4000000)))
+                    .SelectMany(p => p)
+                ).SelectMany(p => p)
+                .First(p => !sensors.Any(s => Math.Abs(s.x - p.x) + Math.Abs(s.y - p.y) <= s.d));
+            return string.Format("{0}/{1}", answer1.Count(), 4000000 * (long)answer2.x + answer2.y);
+        }
+    }
+}
