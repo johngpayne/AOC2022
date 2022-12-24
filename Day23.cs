62eb913814841200a21fc0a68eb5d71465f30b8f
John Payne <20407779+johngpayne@users.noreply.github.com>
2022-12-24 21:50:53 +0000

    Sped up

--- Day23.cs
+++ Day23.cs
@@ -19,28 +19,31 @@ namespace AOC
         public string Calc(string input, bool test)
         {
             var elves = input.Split('\n').Select(line => line.Trim()).Where(line => line != "").Select((line, y) => line.Select((ch, x) => (x, y)).Where(p => line[p.x] == '#')).SelectMany(p => p).ToHashSet();
-            var dirs = new (int x, int y, int bits)[] { (0, -1, 448), (0, 1, 7), (-1, 0, 292), (1, 0, 73) };
+            var dirs = new (int x, int y, int bits)[] { (0, -1, 224), (0, 1, 7), (-1, 0, 148), (1, 0, 41) };
             var ret1 = 0;
             var ret2 = 0;
+            var offsets = Enumerable.Range(-1, 3).Select(y => Enumerable.Range(-1, 3).Select(x => (x,y))).SelectMany(p => p).Where(p => p.x != 0 || p.y != 0).ToArray();
+            var numElves = elves.Count();
             while(true)
             {
                 var elfMoves =
                     elves
-                    .Select(elf => (elf, Enumerable.Range(elf.y - 1, 3).Select(y => Enumerable.Range(elf.x - 1, 3).Select(x => (x,y))).SelectMany(p => p).Aggregate(0, (agg, p) => agg * 2 + (elves.Contains(p) ? 1 : 0))))
-                    .Select(p => p.Item2 == 16 ? (elf: p.Item1, x: 0, y: 0, done: true) : dirs.Where(dir => (p.Item2 & dir.bits) == 0).Select(dir => (elf: p.elf, x: dir.x, y: dir.y, done: false)).FirstOrDefault((elf: p.elf, x: 0, y: 0, done: false)))
-                    .Select(p => (done: p.done, originalPos: p.elf, newPos: (x: p.elf.x + p.x, y: p.elf.y + p.y)))
+                    .Select(elf => (elf, offsets.Aggregate(0, (agg, p) => agg * 2 + (elves.Contains((p.x + elf.x, p.y + elf.y)) ? 1 : 0))))
+                    .Select(p => p.Item2 == 0 ? (elf: p.Item1, xo: 0, yo: 0, stopped: true) : dirs.Where(dir => (p.Item2 & dir.bits) == 0).Select(dir => (elf: p.elf, xo: dir.x, yo: dir.y, stopped: false)).FirstOrDefault((elf: p.elf, xo: 0, yo: 0, stopped: false)))
+                    .Select(p => (stopped: p.stopped, originalPos: p.elf, newPos: (x: p.elf.x + p.xo, y: p.elf.y + p.yo)))
                     .GroupBy(p => p.newPos)
                     .ToLookup(g => g.Count())
-                    .Select(g => g.Select(c => c.Select(p => (done: p.done, pos: g.Key == 1 ? p.newPos : p.originalPos))).SelectMany(e => e))
-                    .SelectMany(e => e);
+                    .Select(g => g.Select(c => c.Select(p => (stopped: p.stopped, pos: g.Key == 1 ? p.newPos : p.originalPos))).SelectMany(e => e))
+                    .SelectMany(e => e)
+                    .ToArray();
                 elves = elfMoves.Select(e => e.pos).ToHashSet();
-                dirs = dirs.Skip(1).Concat(new (int x, int y, int bits)[] { dirs.First() }).ToArray();
+                dirs = dirs.Skip(1).Concat(dirs).Take(4).ToArray();
                 ret2++;
                 if (ret2 == 10)
                 {
-                    ret1 = (1 + elves.Max(elf => elf.x) - elves.Min(elf => elf.x)) * (1 + elves.Max(elf => elf.y) - elves.Min(elf => elf.y)) - elves.Count();
+                    ret1 = (1 + elves.Max(elf => elf.x) - elves.Min(elf => elf.x)) * (1 + elves.Max(elf => elf.y) - elves.Min(elf => elf.y)) - numElves;
                 }
-                if (elfMoves.Count(e => e.done) == elves.Count())
+                if (elfMoves.Count(e => e.stopped) == numElves)
                 {
                     break;
                 }
